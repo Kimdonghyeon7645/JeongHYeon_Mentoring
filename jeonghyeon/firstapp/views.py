f2f3977d28d20343e829081fb5381bb0187c718d
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-21 20:17:18 +0900

    docs : update template file

--- jeonghyeon/firstapp/views.py
+++ jeonghyeon/firstapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'firstapp/post_list.html', {})
+    posts = Post.objects.all().order_by('created_date')
+    print(posts)
+    return render(request, 'firstapp/post_list.html', {'posts': posts})
